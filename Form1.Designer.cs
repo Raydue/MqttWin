a7b50528242fb71db835db199de2aba9fb9f6f22
Ray Due <ray@youngtec.com.tw>
2021-02-22 16:31:22 +0800

    可以將checkbox為True的Node，正確連線且訂閱。

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -35,21 +35,24 @@ namespace MqttWin
             this.button2 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // textBox1
             // 
             this.textBox1.AcceptsTab = true;
-            this.textBox1.Location = new System.Drawing.Point(132, 145);
+            this.textBox1.Location = new System.Drawing.Point(132, 59);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(171, 22);
             this.textBox1.TabIndex = 0;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(350, 145);
+            this.button1.Location = new System.Drawing.Point(405, 316);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(82, 25);
+            this.button1.Size = new System.Drawing.Size(96, 38);
             this.button1.TabIndex = 1;
             this.button1.Text = "訂閱/退訂";
             this.button1.UseVisualStyleBackColor = true;
@@ -57,16 +60,16 @@ namespace MqttWin
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(132, 56);
+            this.textBox2.Location = new System.Drawing.Point(132, 12);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(171, 22);
             this.textBox2.TabIndex = 2;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(350, 56);
+            this.button2.Location = new System.Drawing.Point(405, 171);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(82, 22);
+            this.button2.Size = new System.Drawing.Size(96, 33);
             this.button2.TabIndex = 3;
             this.button2.Text = "連線";
             this.button2.UseVisualStyleBackColor = true;
@@ -77,7 +80,7 @@ namespace MqttWin
             this.label1.AutoSize = true;
             this.label1.Enabled = false;
             this.label1.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.label1.Location = new System.Drawing.Point(20, 58);
+            this.label1.Location = new System.Drawing.Point(20, 18);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(106, 16);
             this.label1.TabIndex = 4;
@@ -88,17 +91,48 @@ namespace MqttWin
             this.label2.AutoSize = true;
             this.label2.Enabled = false;
             this.label2.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.label2.Location = new System.Drawing.Point(42, 148);
+            this.label2.Location = new System.Drawing.Point(31, 59);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(50, 16);
             this.label2.TabIndex = 5;
             this.label2.Text = "Topics";
+            // 
+            // treeView1
+            // 
+            this.treeView1.CheckBoxes = true;
+            this.treeView1.Location = new System.Drawing.Point(23, 127);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(330, 297);
+            this.treeView1.TabIndex = 6;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(359, 6);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(107, 31);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(359, 59);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(107, 29);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.treeView1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button2);
@@ -121,6 +155,9 @@ namespace MqttWin
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -19,10 +19,10 @@ namespace MqttWin
         static extern bool AllocConsole();
 
         List<string> TopicsHome = new List<string>();
-        //delegate void txtReturn(String text);
+     
        public static MqttClient client;
         static string clientid;
-
+             
 
         public Form1()
         {
@@ -34,7 +34,6 @@ namespace MqttWin
         {
 
         }
-
        
         static void client_MqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs e)
         {
@@ -44,14 +43,62 @@ namespace MqttWin
             Console.WriteLine($"{DateTime.Now}\t{ReceiveMsg.Split(',').Length}");
             Console.WriteLine("===============================================");
         }
+        List<TreeNode> nodechecked = new List<TreeNode>();
+        List<string> connectchecked = new List<string>();
+
+       
+      
+        void checkednodesMt(TreeNodeCollection nodes)
+        {
+            foreach (TreeNode node in nodes)
+            {
+                if (node.Checked)               //先把想要刪除掉的node加入到list中
+                {
+                    nodechecked.Add(node);
+                }
+                else
+                {
+                    checkednodesMt(node.Nodes);
+                }
+            }
+        }
+
+        void checkedconnectMt(TreeNodeCollection nodes)
+        {
+            foreach (TreeNode node in nodes)
+            {
+                if (node.Checked)               //先把想要刪除掉的node加入到list中
+                {
+                    connectchecked.Add(node.Text) ;
+                   
+                }
+                else
+                {
+                    checkedconnectMt(node.Nodes);
+                    
+                }
+            }
+        }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            if (textBox1.Text != "")
-            {
-                string topic = textBox1.Text;
 
-                if (TopicsHome.Contains(topic))
+            for(int i = 0; i <= treeView1.Nodes.Count; i++)
+            {
+                if (treeView1.Nodes[i].Checked)
+                {
+                    checkednodesMt(treeView1.Nodes[i].Nodes);
+                    break;
+                }
+                
+            }
+            
+            foreach(var sub in nodechecked)
+            {
+                string a = sub.Text;
+                client.Subscribe(new string[] { a }, new byte[] { 0 });
+            }
+               /* if (TopicsHome.Contains(topic))
                 {
                      client.Unsubscribe(new string[] { topic });
                      TopicsHome.Remove(topic);
@@ -60,35 +107,58 @@ namespace MqttWin
                 {
                      client.Subscribe(new string[] { topic }, new byte[] { 0 });
                      TopicsHome.Add(topic);                    
-                }
+                }*/
                 
-            }
-            else
-            {
-               MessageBox.Show("請輸入要訂閱的主題!!");
-            }
-
-                        
+                                  
         }
 
         
 
         private void button2_Click(object sender, EventArgs e)
         {
-            string gateway = textBox2.Text;
-            client = new MqttClient(gateway);
+                    
+            checkedconnectMt(treeView1.Nodes);
+            client = new MqttClient(connectchecked.Last());
             clientid = Guid.NewGuid().ToString();
 
             client.Connect(clientid);
-            MessageBox.Show(gateway+" 已連線!");
+            MessageBox.Show(connectchecked.Last()+" 已連線!");
             client.MqttMsgPublishReceived += client_MqttMsgPublishReceived;
             client.ConnectionClosed += Form1_Formclosing;
+
+            for(int i = 0; i < treeView1.Nodes.Count; i++)
+            {
+                if (treeView1.Nodes[i].Checked)
+                {
+                    treeView1.Nodes[i].Tag = client;
+                    break;
+                }                  
+            }           
         }
        
         private void Form1_Formclosing(object sender, EventArgs e)
         {
             client.Disconnect();
         }
-      
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string a = textBox2.Text;    
+            treeView1.Nodes.Add(a);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string topic = textBox1.Text;
+            try
+            {
+                treeView1.SelectedNode.Nodes.Add(topic);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("請點選Broker!!");
+            }
+            
+        }
     }
 }
